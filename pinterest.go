efb50750a7ff76208a794711106630672be88018
BrandonRomano <BrandonRRomano@gmail.com>
2016-12-21 10:04:18 -0500

    Reformat imports

--- pinterest.go
+++ pinterest.go
@@ -1,11 +1,10 @@
 package pinterest
 
 import (
-	"net/http"
-	"time"
-
 	"github.com/BrandonRomano/wrecker"
 	"github.com/carrot/go-pinterest/controllers"
+	"net/http"
+	"time"
 )
 
 // Client is an API client that connects you with the
