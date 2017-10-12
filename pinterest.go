e8034c2906b90575ee99239c05e20f8875a53e58
Brandon Romano <BrandonRRomano@gmail.com>
2017-10-12 16:56:49 -0400

    Fix breaking changes

--- pinterest.go
+++ pinterest.go
@@ -1,10 +1,11 @@
 package pinterest
 
 import (
-	"github.com/BrandonRomano/wrecker"
-	"github.com/carrot/go-pinterest/controllers"
 	"net/http"
 	"time"
+
+	"github.com/BrandonRomano/wrecker"
+	"github.com/carrot/go-pinterest/controllers"
 )
 
 // Client is an API client that connects you with the
