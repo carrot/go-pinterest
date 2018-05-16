dac5e6671bb929422b2245cfcbed092dcc2f305e
Rizhiy Olexandr <olexandr.rizhiy@gmail.com>
2018-05-16 23:04:29 +0300

    fix error (#40)
    * add rate limits to error wrapper
    
    * Update rate_limit.go
    
    * Replace Refresh <--->Remaining
    
    Replace Refresh <--->Remaining
    
    * Update rate_limit.go
    
    * fix model/rate_linit.go

--- models/rate_limit.go
+++ models/rate_limit.go
@@ -15,7 +15,7 @@ type TypeRatelimit struct {
 func GetLimit(httpResp *http.Response, key string) int {
 	if val, ok := httpResp.Header[key]; ok {
 		rateLimit, err := strconv.Atoi(val[0])
-		if err == nil {
+		if err != nil {
 			fmt.Println(err)
 			return 0
 		}
@@ -26,8 +26,9 @@ func GetLimit(httpResp *http.Response, key string) int {
 
 func GetRatelimit(httpResp *http.Response) TypeRatelimit {
 	return TypeRatelimit {
-		Remaining: GetLimit(httpResp, "X-Ratelimit-Refresh"),
+		Remaining: GetLimit(httpResp, "X-Ratelimit-Remaining"),
 		Limit: GetLimit(httpResp, "X-Ratelimit-Limit"),
-		Refresh: GetLimit(httpResp, "X-Ratelimit-Remaining"),
+		Refresh: GetLimit(httpResp, "X-Ratelimit-Refresh"),
 	}
 }
+
