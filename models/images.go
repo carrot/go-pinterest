fc9e689bc391dcec8ccde76588776ba2dd98032e
Jens Schwehn <JSchwehn@users.noreply.github.com>
2019-05-21 18:00:44 +0200

    Update images.go

--- models/images.go
+++ models/images.go
@@ -1,9 +1,11 @@
 package models
 
+// 60x60 images
 type Images struct {
 	Size_60x60 Image `json:"60x60"`
 }
 
+// Image with width and height
 type Image struct {
 	Url    string `json:"url"`
 	Width  int32  `json:"width"`
